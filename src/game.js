45f200e8135f2524100079b95836d8df9126b11f
Brandan Sayarath and Michelle Morin <brandan@>
2020-02-04 09:51:21 -0800

    add assignid and addplayer prototypes

--- src/game.js
+++ src/game.js
@@ -3,3 +3,12 @@ export function Game() {
   this.currentId = 0;
   this.currentPlayer = 1;
 }
+
+Game.prototype.assignId = function() {
+  this.currentId += 1;
+};
+
+Game.prototype.addPlayer = function(player) {
+  player.id = this.assignId();
+  this.players.push(player);
+};
