96070e49effaa52cbd40e35009321b6b803f5b69
Brandan Sayarath and Michelle Morin <michelle@>
2020-02-04 09:57:13 -0800

    add setup between tests

--- __tests__/pig-dice-test.js
+++ __tests__/pig-dice-test.js
@@ -3,40 +3,47 @@ import { Player } from '../src/player.js';
 
 describe('Game', () => {
 
+  beforeEach(() => {
+    game = new Game();
+  });
+
   test('should correctly instantiate a game with no arguments', () => {
-    var game = new Game();
     expect(game.players).toEqual([]);
     expect(game.currentId).toEqual(0);
     expect(game.currentPlayer).toEqual(1);
   });
 
   test('should add two player objects to game object', () => {
-    var game = new Game();
     var player1 = new Player("Brandan");
     var player2 = new Player("Michelle");
     game.addPlayer(player1);
     game.addPlayer(player2);
     expect([player1, player2]).toEqual(expect.arrayContaining(game.players));
-  })
+  });
 });
 
 describe ('Player', () => {
 
+  beforeEach(() => {
+    game = new Game();
+    player1 = new Player("Michelle");
+    player2 = new Player("Brandan");
+  });
+
   test('should correctly make two player objects', () => {
-    var player1 = new Player("Michelle");
-    var player2 = new Player("Brandan");
     expect(player1.name).toEqual("Michelle");
     expect(player2.name).toEqual("Brandan");
   });
 
+  test('should switch to other player turn when player rolls 1', () => {
+  });
+
   // test('should return random number when user rolls dice', () => {
-  //   var player1 = new Player("Michelle");
   //   player1.rollDice();
   //   expect(player1.turnScore).toBeGreaterThan(0);
   // });
 
   // test('should change turnscore to 0 when user rolls 1', () => {
-  //   var player1 = new Player("Brandan");
   //   player1.rollDice();
   //   expect(player1.turnScore).toEqual(0);
   // });
