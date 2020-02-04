0ca63abe3df6d98d229d305b37f8be3e07f1620f
Brandan Sayarath and Michelle Morin <michelle@>
2020-02-04 09:51:55 -0800

    pass new spec for adding player objects to game object

--- __tests__/pig-dice-test.js
+++ __tests__/pig-dice-test.js
@@ -9,6 +9,15 @@ describe('Game', () => {
     expect(game.currentId).toEqual(0);
     expect(game.currentPlayer).toEqual(1);
   });
+
+  test('should add two player objects to game object', () => {
+    var game = new Game();
+    var player1 = new Player("Brandan");
+    var player2 = new Player("Michelle");
+    game.addPlayer(player1);
+    game.addPlayer(player2);
+    expect([player1, player2]).toEqual(expect.arrayContaining(game.players));
+  })
 });
 
 describe ('Player', () => {
@@ -24,11 +33,11 @@ describe ('Player', () => {
   //   var player1 = new Player("Michelle");
   //   player1.rollDice();
   //   expect(player1.turnScore).toBeGreaterThan(0);
-  // })
+  // });
 
-  test('should change turnscore to 0 when user rolls 1', () => {
-    var player1 = new Player("Brandan");
-    player1.rollDice();
-    expect(player1.turnScore).toEqual(0);
-  }) 
+  // test('should change turnscore to 0 when user rolls 1', () => {
+  //   var player1 = new Player("Brandan");
+  //   player1.rollDice();
+  //   expect(player1.turnScore).toEqual(0);
+  // });
 });
