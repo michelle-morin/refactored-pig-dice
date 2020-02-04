030c6efbc1ee21827e6c995ade0f926debb48556
Brandan Sayarath and Michelle Morin <brandan@>
2020-02-04 11:01:41 -0800

    add UI logic

--- src/main.js
+++ src/main.js
@@ -2,3 +2,31 @@
 // import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import './styles.css';
+
+$(document).ready(function() {
+  $("button#start-play").click(function(event) {
+    event.preventDefault();
+    var player1Name = $("input#player1Name").val();
+    var player2Name = $("input#player2Name").val();
+
+    var player1 = new Player(player1Name);
+    var player2 = new Player(player2Name);
+    game.addPlayer(player1);
+    game.addPlayer(player2);
+    $("#initialScreen").hide();
+    $("#gameScreen").show();
+    $("#name1").html(player1Name);
+    $("#name2").html(player2Name);
+
+    $("button#roll").click(function(event){
+      event.preventDefault();
+      var playerRolling = game.findPlayer(game.currentPlayer);
+      playerRolling.rollDice(game);
+    });
+
+    $("button#hold").click(function(event) {
+      event.preventDefault();
+      playerRolling.endTurn(game);
+    });
+  });
+});
