1b4f7dfc60770a1dcd00c8b9f0344977900ac375
Brandan Sayarath and Michelle Morin <michelle@>
2020-02-04 10:23:13 -0800

    refactor standalone functions as prototypes

--- src/game.js
+++ src/game.js
@@ -12,3 +12,22 @@ Game.prototype.addPlayer = function(player) {
   player.id = this.assignId();
   this.players.push(player);
 };
+
+Game.prototype.findPlayer = function(id) {
+  for (var i=0; i< this.players.length; i++) {
+    if (this.players[i]) {
+      if (this.players[i].id == id) {
+        return this.players[i];
+      }
+    }
+  };
+  return false;
+}
+
+Game.prototype.switchPlayers = function() {
+  if (this.currentPlayer === 1) {
+    this.currentPlayer = 2;
+  } else if (this.currentPlayer === 2) {
+    this.currentPlayer = 1;
+  }
+}
--- src/player.js
+++ src/player.js
@@ -5,10 +5,17 @@ export function Player(name) {
 }
 
 Player.prototype.rollDice = function() {
-  var diceRoll = Math.floor( Math.random() * 6) +1;
+  // var diceRoll = Math.floor( Math.random() * 6) +1;
+  var diceRoll = 1;
   if (diceRoll == 1) {
     this.turnScore = 0;
+    endTurn(this.id);
   } else {
     this.turnScore += diceRoll;
   }
+};
+
+Player.prototype.endTurn = function() {
+  this.totalScore += this.turnScore;
+  this.turnScore = 0;
 };
