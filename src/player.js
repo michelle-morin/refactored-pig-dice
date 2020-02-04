bfb7117ade2d8061085b3a578f54a54ed114dd01
Brandan Sayarath and Michelle Morin <brandan@>
2020-02-04 10:46:49 -0800

    refactor rolldice and endturn prototypes

--- src/player.js
+++ src/player.js
@@ -2,20 +2,24 @@ export function Player(name) {
   this.name = name;
   this.totalScore = 0;
   this.turnScore = 0;
+  this.win = false;
 }
 
-Player.prototype.rollDice = function() {
+Player.prototype.rollDice = function(game) {
   var diceRoll = Math.floor( Math.random() * 6) +1;
-  // var diceRoll = 1;
   if (diceRoll == 1) {
     this.turnScore = 0;
-    endTurn(this.id);
+    this.endTurn(game);
   } else {
     this.turnScore += diceRoll;
   }
 };
 
-Player.prototype.endTurn = function() {
+Player.prototype.endTurn = function(game) {
   this.totalScore += this.turnScore;
   this.turnScore = 0;
+  if (this.totalScore >= 100) {
+    this.win = true;
+  }
+  game.switchPlayers();
 };
